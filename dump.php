3209c89a85eab7ce2539dfe5c4cb171c2c03a0ed
samtcv <samueljdero@gmail.com>
2024-10-02 13:50:23 -0400

    added php page

---
+++ dump.php
@@ -0,0 +1,13 @@
+<DOCTYPE HTML>
+    <html>
+        <head>
+            <title>dump</title>
+        </head>
+
+        <body>
+
+            <p> <?= var_dump($_SERVER) ?> </p>
+
+        </body>
+
+    </html>
