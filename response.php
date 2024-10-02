56e6d13453d457fbdf0c2be030dcc7a5764f8d4b
samtcv <samueljdero@gmail.com>
2024-10-02 15:12:25 -0400

    Added to response page
    response page with user friendly text

--- response.php
+++ response.php
@@ -6,8 +6,22 @@
 
         <body>
 
-            <p> GET: <?= var_dump($_GET) ?> </p>
-            <p> POST: <?= var_dump($_POST) ?> </p>
+            <p> Welcome <?= htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname']) ?> 
+            to the BEERLYMPIC! ...or should I say <?= html special chars($_POST['gtag']) ?>!!
+            </p> <br>
+
+            <p> You've selected that you <br>
+            <?= htmlspecialchars($_POST['ride1']) . " " . htmlspecialchars($_POST['ride2']) . " " .
+            htmlspecialchars($_POST[ride3]) ?> <br>
+            a driver
+            </p> <br>
+
+            <p> Get ready for lots of fun activities and lots of <?= htmlspecialchars($_POST['robot']) ?>!! 
+            But make sure to <?= htmlspecialchars($_POST['agreement']) ?>!
+            And don't drink if your underage! We have you birthday: <br>
+            <?= htmlspecialchars($_POST['birthday']) ?>
+            </p> <br>
+
 
         </body>
 
