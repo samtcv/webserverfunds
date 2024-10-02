65e68dd70eb79dec2c4dcb3e6e8bdcef4bdabb40
samtcv <samueljdero@gmail.com>
2024-10-02 14:30:07 -0400

    Added response page
    Added response page for signup as well and text box in index

---
+++ response.php
@@ -0,0 +1,14 @@
+<DOCTYPE HTML>
+    <html>
+        <head>
+            <title>dump</title>
+        </head>
+
+        <body>
+
+            <p> GET: <?= var_dump($_GET) ?> </p>
+            <p> POST: <?= var_dump($_POST) ?> </p>
+
+        </body>
+
+    </html>
