c87dbb588219a21032e03b9e4a415811abd89769
samtcv <samueljdero@gmail.com>
2024-10-02 15:18:29 -0400

    Fixed syntax in response page

--- response.php
+++ response.php
@@ -5,14 +5,14 @@
         </head>
 
         <body>
-
+            <div>
             <p> Welcome <?= htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname']) ?> 
             to the BEERLYMPIC! ...or should I say <?= html special chars($_POST['gtag']) ?>!!
             </p> <br>
 
             <p> You've selected that you <br>
             <?= htmlspecialchars($_POST['ride1']) . " " . htmlspecialchars($_POST['ride2']) . " " .
-            htmlspecialchars($_POST[ride3]) ?> <br>
+            htmlspecialchars($_POST['ride3']) ?> <br>
             a driver
             </p> <br>
 
@@ -21,7 +21,7 @@
             And don't drink if your underage! We have you birthday: <br>
             <?= htmlspecialchars($_POST['birthday']) ?>
             </p> <br>
-
+            </div>
 
         </body>
 
